542d65549a7b8e1b5d1d617c6498c007e3bd9020
SpeckyYT <Specky.fans@gmail.com>
2021-09-08 22:19:28 +0200

    fucked up ephf's code

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
+require('colors');
 const { execSync } = require("child_process");
-const { watch } = require("fs");
-const file = process.argv[process.argv.length - 1].replace(
-  /\.spwn$|$/,
-  ".spwn"
-);
-const flags = process.argv.slice(2, process.argv.length - 1).join(" ");
+const { watch } = require("chokidar");
 
-var exTimeout;
+const FLLW = `${"FLLW".green} ${">>".cyan}`;
 
-watch(".", (event, fname) => {
-  if (/\.spwn$/.test(fname)) {
-    if (event == "change") {
-      if (exTimeout) {
-        clearTimeout(exTimeout);
-      }
-      exTimeout = setTimeout(() => {
-        console.log(`FLLW >> Detected changes in "${file}", running spwn...`);
-        try {
-          if (execSync("tasklist").toString().indexOf("GeometryDash.exe") + 1) {
-            execSync("taskkill /F /IM GeometryDash.exe", { stdio: "inherit" });
-            console.log("FLLW >> Closed Geometry Dash.");
-          }
-          execSync(`spwn build ${file} ${flags}`, { stdio: "inherit" });
-          console.log(`FLLW >> Finished building, running Geometry Dash...`);
-          execSync("start steam://rungameid/322170");
-        } catch (e) {
-          console.log(
-            `FLLW >> Error while building, waiting for new changes...`
-          );
+const file = process.argv.length > 2 ?
+  process.argv[2].replace(/(\.spwn)?$/i,'.spwn') :
+  false
+
+const flags = process.argv
+  .slice(2)
+  .join(" ");
+
+const event = watch(file || ".", {depth: Infinity});
+
+event.on('all', (event, fname) => {
+  if (!/\.spwn$/i.test(fname)) return;
+  if (event == "change") {
+    console.log(this.exTimeout)
+    if (this.exTimeout) clearTimeout(this.exTimeout);
+    this.exTimeout = setTimeout(() => {
+      console.log(`${FLLW} Detected changes in "${fname}", running spwn...`);
+      try {
+        if (execSync("tasklist").toString().indexOf("GeometryDash.exe") + 1) {
+          execSync("taskkill /F /IM GeometryDash.exe", { stdio: "inherit" });
+          console.log(`${FLLW} Closed Geometry Dash.`);
         }
-      }, 100);
-    } else if (fname == file) {
-      console.log(
-        `FLLW >> Detected rename or delete of "${file}", closing FLLW...`
-      );
-      process.exit();
-    }
+        execSync(`spwn build ${fname} ${flags}`, { stdio: "inherit" });
+        console.log(`${FLLW} Finished building, running Geometry Dash...`);
+        execSync("start steam://rungameid/322170");
+      } catch (e) {
+        console.log(`${FLLW} Error while building, waiting for new changes...`);
+      }
+    }, 100);
+  } else if (fname == file && event == 'unlink') {
+    console.log(`${FLLW} Detected rename or delete of "${file}", closing FLLW...`);
+    process.exit();
   }
 });
 
-console.log(`FLLW >> Ready and waiting for changes to "${file}"`);
+console.log(`${FLLW} Ready and waiting for changes to ${file ? `"${file}"` : "any spwn file"}`);
